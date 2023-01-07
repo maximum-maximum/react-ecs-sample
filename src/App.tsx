e6d94c19be36b6235e1173926e3e3f6bb29bbfd9
Koki Makishima <mr_ver74@yahoo.co.jp>
2023-01-07 15:38:18 +0900

    check CI/CD

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <a>React ECS Sample</a>
       </header>
     </div>
   );
